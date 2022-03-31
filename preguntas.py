cb5c0214fb29a874f14de4816346e768a4e873a6
Wilson Andrés Pinillo <stwpinillo@gmail.com>
2022-03-30 20:15:39 -0500

    Correcion pregunta 6

--- preguntas.py
+++ preguntas.py
@@ -104,7 +104,7 @@ def pregunta_06():
     ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 
     """
-    lista = tbl1['_c4'].sort_values().str.upper().unique()
+    lista = tbl1['_c4'].sort_values().str.upper().unique().tolist()
     
     return lista
 
